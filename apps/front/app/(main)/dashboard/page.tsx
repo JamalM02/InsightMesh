d7f56133de9b2c30832d149882f0a3b4cc91f083
Jamal Majadle <jamal.majadle02@students.kinneret.ac.il>
2025-07-21 17:37:37 +0300

    feat: add dynamic key to iframe based on current pathname for improved rendering

--- apps/front/app/(main)/dashboard/page.tsx
+++ apps/front/app/(main)/dashboard/page.tsx
@@ -2,8 +2,11 @@
 
 import Breadcrumbs from "@/components/breadcrumbs";
 import { cn } from "@/lib/utils";
+import {usePathname} from "next/navigation";
 
 export default function Page() {
+    const pathname = usePathname();
+
     return (
         <>
             <Breadcrumbs
@@ -14,6 +17,7 @@ export default function Page() {
             />
             <h1 className={cn("text-white")}>Dashboard Page</h1>
             <iframe
+                key={pathname}
                 className="px-5 py-10 h-full"
                 src={process.env.NEXT_PUBLIC_METABASE_DASHBOARD_URL}
                 frameBorder="0"
