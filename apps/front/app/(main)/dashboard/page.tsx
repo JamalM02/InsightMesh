259e46c99dc7457496fbdd8e9d6629bdf8160c19
Jamal Majadle <jamal.majadle02@students.kinneret.ac.il>
2025-07-21 17:15:14 +0300

    feat: enhance dashboard page with iframe attributes for better performance

--- apps/front/app/(main)/dashboard/page.tsx
+++ apps/front/app/(main)/dashboard/page.tsx
@@ -1,21 +1,25 @@
+'use client';
+
 import Breadcrumbs from "@/components/breadcrumbs";
 import { cn } from "@/lib/utils";
 
-
-export default async function Page() {
-  return (
-    <>
-      <Breadcrumbs
-        items={[
-          { title: "Main", url: "" },
-          { title: "Home", url: "/dashboard" },
-        ]}
-      />
-      <h1 className={cn("text-white")}>Dashboard Page</h1>
-      <iframe
-        className="px-5 py-10 h-full"
-        src={process.env.NEXT_PUBLIC_METABASE_DASHBOARD_URL}
-      />
-    </>
-  );
+export default function Page() {
+    return (
+        <>
+            <Breadcrumbs
+                items={[
+                    { title: "Main", url: "" },
+                    { title: "Home", url: "/dashboard" },
+                ]}
+            />
+            <h1 className={cn("text-white")}>Dashboard Page</h1>
+            <iframe
+                className="px-5 py-10 h-full"
+                src={process.env.NEXT_PUBLIC_METABASE_DASHBOARD_URL}
+                frameBorder="0"
+                allowFullScreen
+                loading="lazy"
+            />
+        </>
+    );
 }
